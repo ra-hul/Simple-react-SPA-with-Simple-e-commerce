5a69ea20c96d48abbd5c41df8c18968f82d37e84
Rahul Paul <rahulshanto008@gmail.com>
2021-12-02 21:20:06 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+const Header = () => {
+    return (
+        <div>
+            <h2>This is Header</h2>
+            <img src={logo} alt="" />
+
+        </div>
+    );
+};
+
+export default Header;
