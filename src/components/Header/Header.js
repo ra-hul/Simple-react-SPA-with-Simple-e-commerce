fb535818d47aa5d98f05c005861c01b59eb4100c
Rahul Paul <rahulshanto008@gmail.com>
2021-12-02 21:56:14 +0600

    shop,order,inventroy added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from '../../images/logo.png';
+import './Header.css';
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
-            <img src={logo} alt="" />
-
+        <div className='header'>
+            <img className='logo' src={logo} alt="" />
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/orders">Order Review</a>
+                <a href="/invetor">Manage Inventory</a>
+            </nav>
         </div>
     );
 };
