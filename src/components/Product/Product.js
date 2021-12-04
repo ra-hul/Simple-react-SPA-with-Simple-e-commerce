2d1d0aeb3cd58110f3fe495acfc108914e2047ad
Rahul Paul <rahulshanto008@gmail.com>
2021-12-04 14:25:07 +0600

    react favicon added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
 import './Product.css'
 
 const Product = (props) => {
     // console.log(props)
     const { name, img, seller, price, stock } = props.product
+    const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className="product">
             <div>
@@ -16,7 +20,7 @@ const Product = (props) => {
                 <p><small>only {stock} left in stock-order soon</small></p>
                 <button
                     onClick={() => props.handleAddToCart(props.product)}
-                    className="btn-regular">add to cart</button>
+                    className="btn-regular">{cartIcon}add to cart</button>
             </div>
         </div>
     );
