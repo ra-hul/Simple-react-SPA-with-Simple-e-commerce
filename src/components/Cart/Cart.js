5cce3371da950463a7174674063d9c4a11a07a66
Rahul Paul <rahulshanto008@gmail.com>
2021-12-04 14:59:31 +0600

    total,shipping_cost,grandTotal added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,12 +7,23 @@ const Cart = (props) => {
     for (const product of cart) {
         total = total + product.price;
     }
+
+    // const totalReducer = (previous, product) => product.price;
+    // const total = cart.reduce(totalReducer, 0);
+
+    const shipping = 15;
+    const tax = (total + shipping) * 10;
+    const grandTotal = total + shipping + tax;
+
     return (
         <div>
             <h3>Order Summary</h3>
             <h5>Items Orderd: {props.cart.length}</h5>
             <br />
-            <p>Total: {total}</p>
+            <p>Total: {total.toFixed(2)}</p>
+            <p>Shipping: {shipping}</p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
 
 
 
